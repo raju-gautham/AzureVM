ccd1338d7cc63fe846d3683a4e070cf64a11a351
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2021-07-21 18:00:46 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,63 @@
+provider "azurerm" {
+  features {}
+subscription_id = var.subscription_id
+client_id = var.client_id
+client_secret = var.client_secret
+tenant_id = var.tenant_id
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "terraformtest"
+  location = "Central India"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "tftest-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "tftest-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "tftest-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "tftest"
+  admin_password                  = "Megantha$1212"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
